a87c6f6b84a44ced68b91ab53ddc9cac819a9ccb
Deonte Manning <deontemanning@Deontes-MacBook-Pro.local>
2020-08-27 08:59:10 -0400

    added databse connection.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 const db = require("knex")({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   },
 });
