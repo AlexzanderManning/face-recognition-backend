94b454db1652edae44ddbabb4c4df0a151860af6
Deonte Manning <deontemanning@Deontes-MacBook-Pro.local>
2020-08-27 08:05:26 -0400

    register controller fix.

--- controllers/register.js
+++ controllers/register.js
@@ -1,11 +1,9 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
-  if(!email || !name || !password){
-    return res.status(400).json('Incorrect from submisson.')
+  if (!email || !name || !password) {
+    return res.status(400).json("incorrect form submission");
   }
   const hash = bcrypt.hashSync(password);
-
-  //Create a transaction whe you have to update more than one table at once.
   db.transaction((trx) => {
     trx
       .insert({
@@ -29,8 +27,4 @@ const handleRegister = (req, res, db, bcrypt) => {
       .then(trx.commit)
       .catch(trx.rollback);
   }).catch((err) => res.status(400).json("unable to register"));
-};
-
-module.exports = {
-  handleRegister
 };
